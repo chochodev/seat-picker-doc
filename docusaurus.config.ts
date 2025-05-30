78efa358c5cc9e1028377746c1bcb472516d158b
Emmanuel Michael <michochoprogrammer@gmail.com>
2025-05-30 01:29:06 +0100

    update: checking updates on production

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,13 +1,14 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import tailwindPlugin from './plugins/tailwind-config.cjs';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/brand-svg.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -35,6 +36,7 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
+  plugins: [tailwindPlugin],
 
   presets: [
     [
